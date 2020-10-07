37b58552798762d67ab45449c9023861c8ddb34d
1683177086@qq.com <a1549802511>
2020-10-07 23:39:34 +0800

    堆排序

---
+++ leetcode/Heap.java
@@ -0,0 +1,35 @@
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
+public class Heap {
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        int arr[]={3,2,1,5};
+        int k=2;
+        System.out.println(Arrays.toString(heap.getLeastNumbers(arr,k)));
+    }
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // 默认是小根堆，实现大根堆需要重写一下比较器。
+        Queue<Integer> pq = new PriorityQueue<>((v1, v2) -> v2 - v1);
+        for (int num: arr) {
+            if (pq.size() < k) {
+                pq.offer(num);
+            } else if (num < pq.peek()) {
+                pq.poll();
+                pq.offer(num);
+            }
+        }
+        
+        // 返回堆中的元素
+        int[] res = new int[pq.size()];
+        int idx = 0;
+        for(int num: pq) {
+            res[idx++] = num;
+        }
+        return res;
+    }
+}
